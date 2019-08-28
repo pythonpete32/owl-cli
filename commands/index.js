fcf0484a1be5bf443d84ceca0f1866a6eb745295
python pete <pythonpete32@gmail.com>
2019-08-28 14:41:24 +0100

    first commit

---
+++ commands/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'ink';
+
+/// Hello world command
+const App = () => <Text>Hello World!</Text>;
+
+
+export default App;
